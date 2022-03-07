ac54aa49a0b0571a2dc88cecfc3af5052615210c
yash-patel268 <yashp2126@gmail.com>
2022-03-06 21:08:38 -0500

    Added sudokuSolver function

--- sudoku.c
+++ sudoku.c
@@ -24,25 +24,66 @@ int checkNum(int row, int column, int num){
 	int rowStart = (row/3) * 3;
 	int columnStart = (column/3)*3;
 
-	int flag = 1;
+	int returnValue = 1;
 
 	for (int x = 0; x < N; x++) {
 		if (sudoku[row][x] == num) {
-			flag = 0;
+		    returnValue = 0;
 		}
 		
 		if (sudoku[x][column] == num) {
-			flag = 0;
+			returnValue = 0;
 		}
 
 		for (int x = rowStart; x < rowStart+3; x++){
 			for (int y = columnStart; y < 3; y++) {
 				if (sudoku[x][y] == num ) {
-					flag = 0;
+					returnValue = 0;
 
 				}
 			}
 		}
 	}
-	return flag;
+	return returnValue;
 }
+
+int sudokuSolver(int row, int col) {
+    if (sudoku[row][col] == 0) {
+        for(int x=1; x<=N; x++){
+            if (checkNum(row, col, x) == 1){
+                
+                sudoku[row][col] = x;
+    
+                if ((row+1) < N) {
+                    
+                    if (solver(row + 1, col) == 1) {
+                        return 1;
+                    } else {
+                        
+                        sudoku[row][col] = 0;
+                    }
+                
+                } else if ((col+1)<9) {
+                    if (solver(0, col + 1) == 1) {
+                        return 1;
+                    } else {
+                        
+                        sudoku[row][col] = 0;
+                    }
+                } else { 
+                    return 1;
+                }
+            }
+        }
+    } else {
+        if((row+1) < N) {
+            return solver(row +1, col);
+        } else if((col+1) < N) {
+            return solver(0, col+1);
+        } else {
+            return 1;
+        }
+    } 
+    return 0;
+}
+
