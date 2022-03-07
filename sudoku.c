d5c16e31180a649eaf787cfe2564cd99d2b70951
yash-patel268 <yashp2126@gmail.com>
2022-03-06 20:38:13 -0500

    Added readPuzzle function

--- sudoku.c
+++ sudoku.c
@@ -5,5 +5,19 @@
 
 #define N 9
 
-int sudoku[N][N]={};
+int sudoku[N][N];
+int isCorrect[3] = {0, 0, 0};
+
+typedef struct {
+	int column,row;
+} parameters;
+
+void readPuzzle(FILE *file) {
+	for (int x = 0; x < N; x++) {
+		for (int y = 0; y < N; y++) {
+			fscanf(file, "%d", &sudoku[x][y]);
+		}
+	}
+}
+
 
